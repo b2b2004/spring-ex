5298c1392db0df0e07dc68b9d80f41d528c76eb9
Kwon yongho <b2b2004@nate.com>
2023-03-29 16:44:42 +0900

    1.Test-Case

---
+++ src/main/java/hello/spring/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.spring.service;
+
+import hello.spring.domain.Member;
+import hello.spring.repository.MemberRepository;
+import hello.spring.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    //회원가입
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    //중복 회원 검증
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    //회원 조회
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
+
